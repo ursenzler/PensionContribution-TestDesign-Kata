e9de74540dbde3c6fdd2cc578b1a5fb2312e0bf5
emily <emily@bacheconsulting.com>
2025-05-02 16:36:05 +0200

    reset to start position

---
+++ csharp/PensionContributionCalculations.Test/GoodUnitTest.cs
@@ -0,0 +1,10 @@
+namespace PensionContributionCalculations.Test;
+
+using System;
+using NUnit.Framework;
+
+// TODO: Rename this class to something more appropriate and write some tests here
+[TestFixture]
+public class GoodUnitTest
+{
+}
